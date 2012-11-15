537c79999f7dfe147e5639d4e33b3fa46e6de069
Smotko <ensmotko@gmail.com>
2012-11-15 23:04:13 +0100

    Refactored some code

--- js/game.js
+++ js/game.js
@@ -3,34 +3,30 @@ var game = {
 		
 	},
 	draw : function(gl){
+		
+		var program = Main.program; // Local pointer to shader program
+		
 		gl.enable(gl.DEPTH_TEST);
         gl.clearColor(0, 1, 1, 1);
         gl.clear(gl.STENCIL_BUFFER_BIT | gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.useProgram(Main.program);
-        gl.uniform1i(Main.sampler_location, 0);
+        gl.useProgram(program);
+        gl.uniform1i(program.sampler_location, 0);
         gl.activeTexture(gl.TEXTURE0);
-//        gl.bindTexture(gl.GL_TEXTURE_2D, Main.texture);
         gl.bindTexture(gl.TEXTURE_2D, Main.texture);
         
         var ratio = Main.WIDTH / Main.HEIGHT;
         var model_projection = mat4.lookAt([0,0,-4], [0,0,0], [0,1,0]);
-
         var model_view_projection = mat4.frustum(-ratio, ratio, -1, 1, 3, 7);
-//        Matrix.rotateM(model_projection, 0, -smoothDirection / 1.5f, 0, 1f, 0f);
-//
-//        float[] projection = new float[16];
         var projection = mat4.multiply(model_view_projection, model_projection);
-        gl.uniformMatrix4fv(Main.projectionMatrix_location, false, projection);
+        
+        gl.uniformMatrix4fv(program.projectionMatrix_location, false, projection);
         gl.enable(gl.BLEND);
         gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
         
-        
         // DRAW SQUARE:
-        
         var modelMatrix = mat4.rotate(mat4.identity(), Math.PI, [0, 1, 0]);
-//        var modelMatrix = mat4.identity();
-        gl.uniformMatrix4fv(Main.modelMatrix_location, false, modelMatrix);
-        gl.uniform1f(Main.isText_location, 0.0);
+        gl.uniformMatrix4fv(program.modelMatrix_location, false, modelMatrix);
+        gl.uniform1f(program.isText_location, 0.0);
         
         var vertices = [
                         1.0, 1.0, 0.0,
@@ -42,27 +38,24 @@ var game = {
         vertices = _.map(vertices, function(i){return i*0.5;});
         
         var vertBuffer =  gl.createBuffer();
-        
         gl.bindBuffer(gl.ARRAY_BUFFER, vertBuffer);
-        gl.enableVertexAttribArray(Main.positionLoc);
+        gl.enableVertexAttribArray(program.positionLoc);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        gl.vertexAttribPointer(Main.positionLoc, 3, gl.FLOAT, false, 0, 0);
-        
+        gl.vertexAttribPointer(program.positionLoc, 3, gl.FLOAT, false, 0, 0);
         
         var color = [0,0,0];
         var alpha = 1;
-        var colors = [ color[0], color[1], color[2], alpha, // Top color
-                color[0], color[1], color[2], alpha, // Bottom Left color
-                color[0], color[1], color[2], alpha, // Bottom Right
-                color[0], color[1], color[2], alpha // Transparency
+        var colors = [ color[0], color[1], color[2], alpha, 
+                color[0], color[1], color[2], alpha,
+                color[0], color[1], color[2], alpha,
+                color[0], color[1], color[2], alpha
         ];
         
         var colorBuffer = gl.createBuffer();
-        
-        gl.enableVertexAttribArray(Main.colorLoc);
+        gl.enableVertexAttribArray(program.colorLoc);
         gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-        gl.vertexAttribPointer(Main.colorLoc, 4, gl.FLOAT, false, 0, 0);
+        gl.vertexAttribPointer(program.colorLoc, 4, gl.FLOAT, false, 0, 0);
         
         
         var charIndex = "B".charCodeAt(0) - 32;
@@ -70,38 +63,30 @@ var game = {
         var NUM_SPRITES = 16;
         var uVal = (charIndex % NUM_SPRITES);
         var vVal = Math.floor(charIndex / NUM_SPRITES);
-
-        
         
         var tex = [
                 // Mapping coordinates for the vertices
-                (uVal + 1) * charWidth, vVal * charWidth, // top left (V2)
-                (uVal) * charWidth, vVal * charWidth, // bottom left (V1)
-                (uVal + 1) * charWidth, (vVal + 1) * charWidth, // top right
+                (uVal + 1) * charWidth, vVal * charWidth, 
+                (uVal) * charWidth, vVal * charWidth,
+                (uVal + 1) * charWidth, (vVal + 1) * charWidth, 
                 uVal * charWidth, (vVal + 1) * charWidth, 
-                                                                // (V4)
         ];
-        
-
         
         var texBuffer = gl.createBuffer();
         
-        gl.enableVertexAttribArray(Main.texLoc);
+        gl.enableVertexAttribArray(program.texLoc);
         gl.bindBuffer(gl.ARRAY_BUFFER, texBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(tex), gl.STATIC_DRAW);
-        gl.vertexAttribPointer(Main.texLoc, 2, gl.FLOAT, false, 0, 0);
+        gl.vertexAttribPointer(program.texLoc, 2, gl.FLOAT, false, 0, 0);
         
-        gl.uniform1i(Main.sampler_location, 0);
+        gl.uniform1i(program.sampler_location, 0);
         
-        //gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
-        
-
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
         
-        gl.disableVertexAttribArray(Main.positionLoc);
-        gl.disableVertexAttribArray(Main.colorLoc);
+        gl.disableVertexAttribArray(program.positionLoc);
+        gl.disableVertexAttribArray(program.colorLoc);
+        gl.disableVertexAttribArray(program.tecLoc);
         
-//        
 //        // Draw actual stuff:
 //        bg.bgSquare.z = 0.1f;
 //         bg.draw(gl);
--- js/main.js
+++ js/main.js
@@ -3,41 +3,42 @@ Main = {
 		
 	init : function(){
 		this.canvas = $("#the-canvas")[0];
-		gl     = WebGLUtils.setupWebGL(this.canvas);
+		
+		gl = WebGLUtils.setupWebGL(this.canvas);
 		if (!gl) {
-			console.log("Sorry :(");
+			console.log("Sorry, something went wrong :(");
 		}
+		
 		this.WIDTH = this.canvas.width;
 		this.HEIGHT = this.canvas.height;
 		// Create shaders:
+		
 		this.program = ProgramUtils.createProgram($("#shader-vs").html(), $("#shader-fs").html());
-		
-		
-		
-		this.positionLoc = gl.getAttribLocation(this.program, "attribute_Position");
-        this.colorLoc = gl.getAttribLocation(this.program, "attribute_Color");
-        this.texLoc = gl.getAttribLocation(this.program, "a_texCoord");
-		
-		this.projectionMatrix_location = gl.getUniformLocation(this.program, "uniform_Projection");
-	    this.modelMatrix_location = gl.getUniformLocation(this.program, "uniform_Model");
-	    this.sampler_location = gl.getUniformLocation(this.program, "s_texture");
-	    this.isText_location = gl.getUniformLocation(this.program, "u_isText");
+		_.extend(this.program, {
+			positionLoc : gl.getAttribLocation(this.program, "attribute_Position"),
+	        colorLoc : gl.getAttribLocation(this.program, "attribute_Color"),
+	        texLoc : gl.getAttribLocation(this.program, "a_texCoord"),
+			projectionMatrix_location : gl.getUniformLocation(this.program, "uniform_Projection"),
+		    modelMatrix_location : gl.getUniformLocation(this.program, "uniform_Model"),
+		    sampler_location : gl.getUniformLocation(this.program, "s_texture"),
+		    isText_location : gl.getUniformLocation(this.program, "u_isText")
+		});
 		
 	    this.t0 = Date.now();
 	    
 	    var image = new Image();
 		image.src = "res/text.png";
 		image.onload = function(){
-			var texty = gl.createTexture();
-			texty.image = image;
+			var texture = gl.createTexture();
+			texture.image = image;
 			gl.pixelStorei(gl.UNPACK_ALIGNMENT, true);
-			gl.bindTexture(gl.TEXTURE_2D, texty);
-			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texty.image);
+			gl.bindTexture(gl.TEXTURE_2D, texture);
+			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
 			gl.generateMipmap(gl.TEXTURE_2D);
 			gl.bindTexture(gl.TEXTURE_2D, null);
-			Main.texture = texty;
+			Main.texture = texture;
 			Main.render();
 		};
 
@@ -54,7 +55,6 @@ Main = {
 		  
 		game.tick(theta);
 		game.draw(gl);
-		  
 	}
 };
 
